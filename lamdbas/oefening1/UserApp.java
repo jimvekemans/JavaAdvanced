f679d97a8114c64d6566bf717e4862f3cf825437
jimvekemans <11400464@student.pxl.be>
2018-10-08 12:14:48 +0200

    Finished Lambda expressions

--- lamdbas/oefening1/UserApp.java
+++ lamdbas/oefening1/UserApp.java
@@ -1,5 +1,8 @@
 package oefening1;
 
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class UserApp {
@@ -23,12 +26,31 @@ public class UserApp {
         System.out.println(adminUserPredicate.test(secondUser));
         //------------------------------------------------------------------------------
         User thirdUser = new User("Ben", "Admin");
-        
+
         Predicate<User> memberUserStartsWithBPredicate = user -> {
             return user.getRole().equalsIgnoreCase("member") && user.getName().startsWith("B");
         };
 
         System.out.println(memberUserStartsWithBPredicate.test(thirdUser));
+        //------------------------------------------------------------------------------
+        User fourthUser = new User("Benny", "CSGO addict");
+
+        Function<User, String> userGeneratePassword = user -> {
+            String firstTwoLetters = user.getName().substring(0, 2);
+            int numbers = (new Random()).nextInt(9999);
+            return String.format("%s%04d", firstTwoLetters, numbers);
+        };
+
+        System.out.println(userGeneratePassword.apply(fourthUser));
+        //------------------------------------------------------------------------------
+        User fifthUser = new User("Billy", "420 blazer");
+
+        Consumer<User> userConsumer = user -> {
+            StringBuilder tempBuilder = new StringBuilder(user.getName());
+            System.out.println(tempBuilder.reverse());
+        };
+
+        userConsumer.accept(fifthUser);
     }
 
 }
