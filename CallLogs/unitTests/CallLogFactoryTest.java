25f463c882cf4ae317d2a3a42f09ad4155a7b643
jimvekemans <JimVekemans@hotmail.com>
2018-12-18 17:52:51 +0100

    Finished CallLogs exercise

---
+++ CallLogs/unitTests/CallLogFactoryTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CallLogFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void readCallLogsFromFile() {
+    }
+
+    @Test
+    void readCallLogsFromFile1() {
+    }
+
+    @Test
+    void readCallLogsFromFile2() {
+    }
+}
